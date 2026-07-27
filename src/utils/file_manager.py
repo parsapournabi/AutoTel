6be38125024508577e53708b18c1a0b5087ee4a4
Wearily <parsapournabi1382@gmail.com>
2026-07-27 12:55:21 +0330

    initializing files manager methods for FileManager class

--- src/utils/file_manager.py
+++ src/utils/file_manager.py
@@ -9,6 +9,24 @@ class FileManager(QObject):
     def __init__(self, parent=None):
         super(FileManager, self).__init__(parent=parent)
 
+    # Public methods
+    def has_new_files(self, path="") -> bool:
+        _path = path or self._targetPath
+        if not path:
+            print("Path is empty: ", path, self._targetPath)
+            return False
+
+        files = self._get_current_files(_path)
+        if not files:
+            # target path has no such any files
+            return False
+
+        if self._is_files_are_new(files):
+            return True
+        return False
+
+    # Properties
+
     @pyqtProperty(str)
     def targetPath(self) -> str:
         return self._targetPath
@@ -19,3 +37,11 @@ class FileManager(QObject):
             return
         self._targetPath = path
         self.targetPathChanged.emit()
+
+    # Protected & Private methods
+
+    def _get_current_files(self, path: str) -> list:
+        pass
+
+    def _is_files_are_new(self, files: list) -> bool:
+        pass

8078b70e133942befaed68f0528cb29603097876
Wearily <parsapournabi1382@gmail.com>
2026-07-27 15:05:05 +0330

    updating _get_current_files method at FileManager
    fixed: typo on has_new_files method.

--- src/utils/file_manager.py
+++ src/utils/file_manager.py
@@ -1,9 +1,11 @@
 from PyQt5.QtCore import QObject, pyqtProperty, pyqtSignal
+from global_logic import iter_files
 import os
+import inspect
 
 
 class FileManager(QObject):
-    _targetPath: str = os.path.expanduser("~/Pictures/Screenshots")
+    _targetPath: str = os.path.join(os.path.expanduser("~"), "Pictures/Screenshots")
     targetPathChanged = pyqtSignal()
 
     def __init__(self, parent=None):
@@ -12,7 +14,7 @@ class FileManager(QObject):
     # Public methods
     def has_new_files(self, path="") -> bool:
         _path = path or self._targetPath
-        if not path:
+        if not _path:
             print("Path is empty: ", path, self._targetPath)
             return False
 
@@ -41,7 +43,23 @@ class FileManager(QObject):
     # Protected & Private methods
 
     def _get_current_files(self, path: str) -> list:
-        pass
+        try:
+            if not os.path.exists(path):
+                print("Target path isn't exists!", path)
+                return []
+            if not os.path.isdir(path):
+                print("Target path isn't directory", path)
+                return []
+            return list(iter_files(path))
+
+        except Exception as ex:
+            print(f"Exception {self.__class__.__name__}.{inspect.currentframe().f_code.co_name}\n{ex}")
+        return []
 
     def _is_files_are_new(self, files: list) -> bool:
         pass
+
+
+if __name__ == '__main__':
+    fmng = FileManager()
+    fmng.has_new_files()
